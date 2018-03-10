f17ccf130133a89218e3db022c95558308e7cc59
github@cybertekschool.com <JuNiOr@MSI.fios-router.home>
2018-03-10 14:42:47 -0500

    firstone

---
+++ muhammet/src/myproject/myClass.java
@@ -0,0 +1,8 @@
+package myproject;
+
+public class myClass {
+public static void main(String[] args) {
+	System.out.println("hi from jersey");
+	System.out.println("s");
+}
+}
---
+++ muhammet/src/myproject2/myClass2.java
@@ -0,0 +1,7 @@
+package myproject2;
+
+public class myClass2 {
+public static void main(String[] args) {
+	System.out.println("please add one more");
+}
+}
